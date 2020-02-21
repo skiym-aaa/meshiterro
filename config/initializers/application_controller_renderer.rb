be492455dd4463238a55bd13dc2694580074927a
skiym-aaa <y.sakiyama3216@gmail.com>
2020-02-21 18:03:28 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3fdc3019e3ba134b24f01981d37fda9bcf001c7441044ae80423a5acbca406c608b6acde44d6ea2f387715ed4aea8651a2ac66a740cac41a15dd97878fb0dd07'
