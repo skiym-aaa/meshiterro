56a550c79b0871ea63a7f62d5b0c75a467bbe7ec
skiym-aaa <y.sakiyama3216@gmail.com>
2020-03-03 08:41:47 +0000

    [Update]

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,13 +45,13 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 namespace :deploy do
   desc 'Database'
   task :db_migrate do
-    on roles(:app) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:migrate'
-        end
-      end
-    end
+    # on roles(:app) do |host|
+    #   with rails_env: fetch(:rails_env) do
+    #     within current_path do
+    #       execute :bundle, :exec, :rake, 'db:migrate'
+    #     end
+    #   end
+    # end
   end
 end
 after 'bundler:install', 'deploy:db_migrate'
